f7c702ffeef6a7582d97b24af4a8783e147a31d7
basaravikiran <basa.ravikiran@gmail.com>
2023-04-01 19:39:17 +0300

    Update superset-entrypoint.sh
    Added admin credentials

--- superset-entrypoint.sh
+++ superset-entrypoint.sh
@@ -2,5 +2,11 @@
 superset db upgrade
 export FLASK_APP=superset
 superset init
+superset fab create-admin \
+               --username admin \
+               --firstname Superset \
+               --lastname Admin \
+               --email admin@superset.com \
+               --password admin
 #superset run --host=0.0.0.0 -p 8088
 gunicorn -w 3 -k gevent --timeout 120 -b 0.0.0.0:${SUPERSET_PORT} "superset.app:create_app()"
